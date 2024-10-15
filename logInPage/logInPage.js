47571758dc52013fe2edbc214f441a8a1d4233dc
eeng20 <tul40997@temple.edu>
2024-10-15 10:41:17 -0400

    revised TODO comment

--- logInPage/logInPage.js
+++ logInPage/logInPage.js
@@ -2,5 +2,5 @@ function myFunction() {
     var x = document.getElementById("userName").value;
     document.getElementById("demo").innerHTML = x;
 
-    // TO-DO: connect backend with frontend to allow the entry of the username into the database
+    // TO-DO: connect backend with frontend
   }
