31238bdde74e2a5c361646a92a458556e338bf4b
Lilly Hewitt <tuo47895@temple.edu>
2024-10-28 20:41:21 -0400

    add homePage functionality

--- logInPage/logInPage.js
+++ logInPage/logInPage.js
@@ -1,6 +1,6 @@
 function myFunction() {
     var x = document.getElementById("userName").value;
     document.getElementById("demo").innerHTML = x;
-
+    window.location.href = '../homePage/homePage.html';
     // TO-DO: connect backend with frontend
   }
--- signUpPage/signUpPage.js
+++ signUpPage/signUpPage.js
@@ -3,7 +3,7 @@ function myFunction() {
     const y = document.getElementById("userEmail").value;
     document.getElementById("demo2").innerHTML = x;
     document.getElementById("demo").innerHTML = y;
-    
+    window.location.href = '../homePage/homePage.html';
     
     // TO-DO: connect backend with frontend to allow the entry of the username, password, and email into the database
   }
