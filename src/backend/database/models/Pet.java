3ced8db0df38618ecb78793bdef3b0fd56b05650
trevorfTU <143643390+trevorfTU@users.noreply.github.com>
2024-10-31 11:50:54 -0400

    Added constructors+toStrings to DTOs, + testfile

---
+++ src/backend/database/models/Pet.java
@@ -0,0 +1,101 @@
+package models;
+
+
+/**
+ * Represents a pet owned by a user in the database.
+ */
+public class Pet {
+
+    private int petID;
+    private int userID;
+    private String petName;
+    private String petType;
+    private String petPNG;
+    
+
+    /**
+     * Constructors
+     * @param petID
+     * @param userID
+     * @param petName
+     * @param petType
+     * @param petPNG
+     */
+    public Pet(int petID, int userID, String petName, String petType, String petPNG) {
+        this.petID = petID;
+        this.userID = userID;
+        this.petName = petName;
+        this.petType = petType;
+        this.petPNG = petPNG;
+    }
+
+    /**
+     * ToString
+     */
+    @Override
+    public String toString() {
+        return "Pet [petID=" + petID + ", userID=" + userID + ", petName=" + petName + ", petType=" + petType
+                + ", petPNG=" + petPNG + "]";
+    }
+
+    /**
+     * Gets pet ID
+     * @return pet ID
+     */
+    public int getPetID() { return petID; }
+
+    /**
+     * Sets pet ID
+     * @param petID pet ID
+     */
+    public void setPetID(int petID) { this.petID = petID; }
+
+    /**
+     * Gets user ID
+     * @return user ID
+     */
+    public int getUserID() { return userID; }
+
+    /**
+     * Sets user ID
+     * @param userID user ID
+     */
+    public void setUserID(int userID) { this.userID = userID; }
+
+    /**
+     * Gets pet name
+     * @return pet name
+     */
+    public String getPetName() { return petName; }
+
+    /**
+     * Sets pet name
+     * @param petName pet name
+     */
+    public void setPetName(String petName) { this.petName = petName; }
+
+    /**
+     * Gets pet type
+     * @return pet type
+     */
+    public String getPetType() { return petType; }
+
+    /**
+     * Sets pet type
+     * @param petType pet type
+     */
+    public void setPetType(String petType) { this.petType = petType; }
+
+    /**
+     * Gets pet PNG image
+     * @return pet PNG
+     */
+    public String getPetPNG() { return petPNG; }
+
+    /**
+     * Sets pet PNG image
+     * @param petPNG pet PNG
+     */
+    public void setPetPNG(String petPNG) { this.petPNG = petPNG; }
+    
+}
---
+++ src/backend/database/models/Skribble.java
@@ -0,0 +1,118 @@
+package models;
+
+import java.time.LocalDate;
+
+/**
+ * Represents a Skribble journal entry made by a user in the database.
+ */
+public class Skribble {
+    private int entryID;
+    private int userID;
+    private LocalDate dateCreated;
+    private String entry;
+    private String mood;
+    private String aiPrompt;
+
+    /**
+     * Constructors
+     * @param entryID
+     * @param userID
+     * @param dateCreated
+     * @param entry
+     * @param mood
+     * @param aiPrompt
+     */
+
+    public Skribble(int entryID, int userID, LocalDate dateCreated, String entry, String mood, String aiPrompt) {
+        this.entryID = entryID;
+        this.userID = userID;
+        this.dateCreated = dateCreated;
+        this.entry = entry;
+        this.mood = mood;
+        this.aiPrompt = aiPrompt;
+    }
+
+    /**
+     * ToString
+     */
+    @Override
+    public String toString() {
+        return "Skribble [entryID=" + entryID + ", userID=" + userID + ", dateCreated=" + dateCreated + ", entry="
+                + entry + ", mood=" + mood + ", aiPrompt=" + aiPrompt + "]";
+    }
+
+    /**
+     * Gets entry ID
+     * @return entry ID
+     */
+    public int getEntryID() { return entryID; }
+
+    /**
+     * Sets entry ID
+     * @param entryID entry ID
+     */
+    public void setEntryID(int entryID) { this.entryID = entryID; }
+
+    /**
+     * Gets user ID
+     * @return user ID
+     */
+    public int getUserID() { return userID; }
+
+    /**
+     * Sets user ID
+     * @param userID user ID
+     */
+    public void setUserID(int userID) { this.userID = userID; }
+
+    /**
+     * Gets date entry was made
+     * @return date created
+     */
+    public LocalDate getDateCreated() { return dateCreated; }
+
+    /**
+     * Sets date entry was made
+     * @param dateCreated date created
+     */
+    public void setDateCreated(LocalDate dateCreated) { this.dateCreated = dateCreated; }
+
+    /**
+     * Gets entry
+     * @return entry
+     */
+    public String getentry() { return entry; }
+
+    /**
+     * Sets entry
+     * @param entry entry
+     */
+    public void setentry(String entry) { this.entry = entry; }
+
+    /**
+     * Gets user mood
+     * @return mood
+     */
+    public String getMood() { return mood; }
+
+    /**
+     * Sets user mood
+     * @param mood mood
+     */
+    public void setMood(String mood) { this.mood = mood; }
+
+    /**
+     * Gets generated prompt for entry
+     * @return generated prompt
+     */
+    public String getaiPrompt() { return aiPrompt; }
+
+    /**
+     * Sets generated prompt for entry
+     * @param aiPrompt aiPrompt
+     */
+    public void setaiPrompt(String aiPrompt) { this.aiPrompt = aiPrompt; } 
+
+}
+
+
