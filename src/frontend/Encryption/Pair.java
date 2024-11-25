1f14021c4782563eca8fbfcc1b414c5f6a95f48f
Paul Sedra <paulm.sedra@gmail.com>
2024-11-25 17:41:06 -0500

    removing old java files

--- src/frontend/Encryption/Pair.java
+++
@@ -1,9 +0,0 @@
-package src.frontend.Encryption;
-
-public record Pair<T, U>(T first, U second) {
-
-    @Override
-    public String toString() {
-        return "<" + first + ", " + second + ">";
-    }
-}
